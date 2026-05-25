beb666ee7ca71f496627e3cd04f34473042cc864
RudDavid <satorugodze17@gmail.com>
2026-05-25 19:16:04 +0700

    beta/My team project/version 1.0/first comit

---
+++ Our first project/model/TeamModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Our_first_project.model {
+  internal class TeamModel {
+    public string Name { get; }
+
+  }
+}
